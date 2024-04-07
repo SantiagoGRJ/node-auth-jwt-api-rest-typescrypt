ea3e25cc818ceb76f8509d4e002b37e897238f43
SantiagoGRJ <santiago-2003-rojas@hotmail.com>
2024-04-06 22:11:31 -0600

    Realizamos un proyecto node con typescript para el de autenticacion con api rest de usuarios con postgreSQL

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+import authRoutes from './routes/authRoutes'
+import userRoutes from './routes/userRoutes'
+const app = express()
+
+app.use(express.json())
+
+//Routes
+app.use('/auth',authRoutes)
+app.use('/users',userRoutes)
+
+//hacer una api Rest de usuarios
+
+
+
+
+
+
+export default app
